5eb155c65876461761dc7514ce85da2c5fe75832
amallick252 <amallick252@gmail.com>
2024-02-05 22:00:21 +0530

    updated

--- 09theamSwitcher/src/contexts/theme.js
+++ 09theamSwitcher/src/contexts/theme.js
@@ -5,7 +5,7 @@ export const ThemeContext = createContext({
     darkTheme: ()=>{},
     lightTheme: ()=>{}
 })
-//The initial value of the context is an object with three properties: themeMode, darkTheme, and lightTheme.
+//The initial value of the context is an object with three properties: themeMode, darkTheme, and lightTheme. defining object properties initially will help set initial values, if no value have been provided.
 
 export const ThemeProvider = ThemeContext.Provider
 // exports a ThemeProvider component, which is essentially the Provider component of the ThemeContext. It will be used to wrap parts of your application where you want to provide the theme context.
@@ -13,6 +13,6 @@ export const ThemeProvider = ThemeContext.Provider
 export default function useTheme(){
     return useContext(ThemeContext)
 }
-//It uses the useContext hook to consume the ThemeContext. When you call useTheme within a functional component, it will return the current context value, which includes themeMode, darkTheme, and lightTheme
+//It uses the useContext hook to consume the ThemeContext. When you call useTheme within a functional component, it will return the current context value, which includes themeMode, darkTheme, and lightTheme Also you dont need to import useContext and ThemeContext separately, you can just import useTheme instead
 
 // We are using .js file because we are not returning any jsx components
